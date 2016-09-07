cb72174c4fd9e35bc28e0d418d57adf7cc4cc5e5
Keir Sweeney <keir.sweeney@cohaesus.co.uk>
2016-09-07 16:55:42 +0100

    Add Browser Sync and create new Gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -5,30 +5,61 @@ var gulp = require("gulp"),
     plumber = require("gulp-plumber"),
     sass = require("gulp-sass"),
     autoprefixer = require("gulp-autoprefixer"),
-    cleanCSS = require("gulp-clean-css");
+    cleanCSS = require("gulp-clean-css"),
+    browserSync = require("browser-sync").create();
 
 //paths
 var paths = {
   styles: {
     src: './src/scss/**/*.scss',
     dist: './dist/css/'
+  },
+  html: {
+    src: './src/index.html',
+    dist: './dist/html/'
   }
 }
 
 gulp.task('sass', function() {
   return gulp.src(paths.styles.src)
-    // .pipe(plumber)
-    // .pipe(autoprefixer({
-    //   browsers: ['last 2 versions']
-    // }))
+    .pipe(plumber())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(sass())
     .pipe(sass().on('error', sass.logError))
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest(paths.styles.dist));
+    .pipe(gulp.dest(paths.styles.dist))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-// gulp.task('sass:watch', function () {
-//   gulp.watch('./src/scss/**/*.scss', ['sass']);
-// });
+gulp.task('html', function() {
+  return gulp.src(paths.html.src)
+  .pipe(gulp.dest(paths.html.dist))
+
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir : 'dist'
+    },
+    startPath: '/html'
+  });
+});
+
+gulp.task('sass:watch',['sass'], function() {
+  gulp.watch(['src/scss/*.scss','src/scss/**/*.scss'], ['sass']);
+});
+
+gulp.task('browser:watch',['browserSync', 'sass', 'html'], function() {
+  gulp.watch(['src/scss/*.scss','src/scss/**/*.scss'], ['sass']);
+  gulp.watch('src/*.html', ['html']);
+  gulp.watch('src/*.html').on('change', browserSync.reload);
+  //gulp.watch('src/js/**/*.js', browserSync.reload);
+});
 
 gulp.task('default', ['sass']);
 
